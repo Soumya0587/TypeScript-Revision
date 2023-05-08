a7534b0b4199deff2a894091c683fc918829a390
Soumya0587 <ghoshalsoumalya96@gmail.com>
2023-05-09 00:08:47 +0530

    basic type covered

--- chapter-1/build/js/main.js
+++ chapter-1/build/js/main.js
@@ -6,7 +6,7 @@ let b = 6;
 let c = 2;
 console.log(a / b);
 console.log(a / c);
-// implicitly 
+// implicitly
 let myName = "soumalya";
 // explicitly decleared
 let myFatherName = "john";
@@ -20,3 +20,28 @@ const sum = (a, b) => {
     return a + b;
 };
 let re = /\w+/g;
+// Array
+let arr1 = ["india", "usa", "russia"];
+let arr2 = [1, 2, 3, "big"];
+let arr3 = [1, true, "yes"];
+arr1[0] = "spain";
+arr2[2] = "germany";
+// tuple
+let myTuple = ["soumya", 49, false];
+myTuple[0] = "hello";
+console.log(myTuple);
+let myobj = {
+    name: "soumya",
+    height: 5.6,
+    hobby: [1, 2, "painting"]
+};
+// enum
+var uni;
+(function (uni) {
+    uni[uni["u"] = 1] = "u";
+    uni[uni["a"] = 2] = "a";
+    uni[uni["b"] = 3] = "b";
+    uni[uni["v"] = 4] = "v";
+    uni[uni["k"] = 5] = "k";
+})(uni || (uni = {}));
+console.log(uni.v);
--- chapter-1/src/main.ts
+++ chapter-1/src/main.ts
@@ -1,27 +1,66 @@
-let username = "soumya"
+let username = "soumya";
 console.log(username);
 
-let a : number =12
-let b : number =6
-let c : number =2
+let a: number = 12;
+let b: number = 6;
+let c: number = 2;
 
-console.log(a/b);
-console.log(a/c);
+console.log(a / b);
+console.log(a / c);
 
-
-// implicitly 
-let myName = "soumalya" 
+// implicitly
+let myName = "soumalya";
 // explicitly decleared
-let myFatherName : string = "john"
-let age : number
-let loading : boolean
-let album : string | number | boolean
-age =7
-loading = true
-album =true
+let myFatherName: string = "john";
+let age: number;
+let loading: boolean;
+let album: string | number | boolean;
+age = 7;
+loading = true;
+album = true;
 
-const sum =(a: number,b: number)=>{
-    return a+b
+const sum = (a: number, b: number) => {
+  return a + b;
+};
+
+let re: RegExp = /\w+/g;
+
+// Array
+
+let arr1 = ["india", "usa", "russia"];
+let arr2 = [1, 2, 3, "big"];
+let arr3 = [1, true, "yes"];
+
+arr1[0]="spain"
+arr2[2]="germany"
+
+// tuple
+
+let myTuple : [string,number,boolean] = ["soumya",49,false]
+myTuple[0]="hello"
+console.log(myTuple);
+
+// object and type
+
+type player ={
+    name : string,
+    height : number,
+    hobby : (string | number) []
 }
 
-let re: RegExp = /\w+/g
+let myobj : player ={
+    name : "soumya",
+    height : 5.6,
+    hobby : [1,2, "painting"]
+}
+
+// enum
+
+enum uni {
+    u=1,
+    a,
+    b,
+    v,
+    k
+}
+console.log(uni.v);
