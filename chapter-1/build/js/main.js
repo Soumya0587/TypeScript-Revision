82eee34fe0d3a7444425b8674d9dd83fe913404c
Soumya0587 <ghoshalsoumalya96@gmail.com>
2023-05-08 18:08:29 +0530

    datatypes covered

--- chapter-1/build/js/main.js
+++ chapter-1/build/js/main.js
@@ -1,3 +1,22 @@
 "use strict";
 let username = "soumya";
 console.log(username);
+let a = 12;
+let b = 6;
+let c = 2;
+console.log(a / b);
+console.log(a / c);
+// implicitly 
+let myName = "soumalya";
+// explicitly decleared
+let myFatherName = "john";
+let age;
+let loading;
+let album;
+age = 7;
+loading = true;
+album = true;
+const sum = (a, b) => {
+    return a + b;
+};
+let re = /\w+/g;
--- chapter-1/src/main.ts
+++ chapter-1/src/main.ts
@@ -1,2 +1,27 @@
 let username = "soumya"
 console.log(username);
+
+let a : number =12
+let b : number =6
+let c : number =2
+
+console.log(a/b);
+console.log(a/c);
+
+
+// implicitly 
+let myName = "soumalya" 
+// explicitly decleared
+let myFatherName : string = "john"
+let age : number
+let loading : boolean
+let album : string | number | boolean
+age =7
+loading = true
+album =true
+
+const sum =(a: number,b: number)=>{
+    return a+b
+}
+
+let re: RegExp = /\w+/g
